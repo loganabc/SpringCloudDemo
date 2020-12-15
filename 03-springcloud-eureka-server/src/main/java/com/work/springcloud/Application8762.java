788e2ea71d838ff813e760635e9a1f8ac0245d1e
loganabc <c1520416296@126.com>
2020-12-15 15:01:58 +0800

    add code

---
+++ 03-springcloud-eureka-server/src/main/java/com/work/springcloud/Application8762.java
@@ -0,0 +1,13 @@
+package com.work.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application8762 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application8762.class, args);
+    }
+
+}
